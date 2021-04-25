b97a8e167c63032cc22d7df7f136f5b6aae1ab7d
Oleg <brezitskyyoleg@gmail.com>
2021-04-25 13:09:15 +0200

    Añadir Centro y Grado a Admin

--- src/wikiumaApp/admin.py
+++ src/wikiumaApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class CentroAdmin(admin.ModelAdmin):
+    list_display = ["nombre"]
+    search_fields = ["nombre"]
+
+
+class GradoAdmin(admin.ModelAdmin):
+    list_display = ["nombre"]
+    search_fields = ["nombre"]
+
+
+admin.site.register(Centro, CentroAdmin)
+admin.site.register(Grado, GradoAdmin)
