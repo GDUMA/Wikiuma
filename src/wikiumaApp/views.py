5c9d4d572be064ec3e23ee6d4d3e4d98d3555e80
Carlos Javier Gutierrez Sanchez <cgutisanc@gmail.com>
2021-05-08 22:17:32 +0200

    add bootstrap, create base template
    Co-authored-by: Miguel Lillo del Barrio <miguel_lillo96@hotmail.com>

--- src/wikiumaApp/views.py
+++ src/wikiumaApp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class BasePageView(TemplateView):
+    page_title = ""
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = self.page_title
+
+        return context
+
+class HomeView(BasePageView):
+    page_title = "Home"
+    template_name = "home.html"
+
