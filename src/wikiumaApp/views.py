a0b1d620dd6e69a8def63b00495964e232449552
Oleg <brezitskyyoleg@gmail.com>
2021-05-12 19:57:50 +0200

    Added CentroView and Centro detail urls

--- src/wikiumaApp/views.py
+++ src/wikiumaApp/views.py
@@ -1,7 +1,11 @@
+from django.db.models import query
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Centro
 
 # Create your views here.
+
+
 class BasePageView(TemplateView):
     page_title = ""
 
@@ -11,7 +15,29 @@ class BasePageView(TemplateView):
 
         return context
 
+
 class HomeView(BasePageView):
     page_title = "Home"
     template_name = "home.html"
 
+
+class CentroView(ListView):
+    model = Centro
+    template_name = 'centros/centro_list.html'
+    page_title = 'Centros😀'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = self.page_title
+        return context
+
+
+class CentroDetailView(DetailView):
+    model = Centro
+    template_name = 'centros/centro_detail.html'
+    page_title = 'Centros😀'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = self.page_title
+        return context
